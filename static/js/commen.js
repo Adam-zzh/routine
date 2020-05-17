9b55737365b0d46dd331e4cd2dcd2986a68d26a8
zzh <964389099@qq.com>
2020-05-17 13:32:53 +0800

    20200517今天喝了豆腐脑

--- static/js/commen.js
+++
@@ -1,7 +0,0 @@
-$(function{
-	/*bootstrap下拉框选中样式*/
-	$(".dropdown-toggle").bind('click',  function(event) {
-		$(".dropdown-toggle").removeClass('active');
-		$(this).addClass('active');
-	});
-})
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,25 @@
 $(function(){
+	/*菜单收缩样式 经测试三级菜单栏也适用*/
+	$('.menu a').on('click',function(){
+        if (!$(this).hasClass('active')) {
+            //展开未展开
+            var aClassNames = $(this).attr('class');
+            var aClassName = aClassNames.split(" ")[0];
+        	$("."+aClassName+"_submenu").slideUp(300);
+        	$("."+aClassName).removeClass('active');
+        	$("."+aClassName+"_submenu a").removeClass('active');
+            $(this).next('ul').slideDown(300);
+            $(this).addClass('active');
+        }else{
+            //收缩已展开 已展开不管是点击的一级还是二级 所有ul都收缩
+            $(this).next('ul').slideUp(300);
+            $(this).removeClass('active');
+        }
+    })
 
-	$(".dropdown-toggle").bind('click',function(){
+    /*bootstrap下拉框选中样式*/
+	$(".dropdown-toggle").bind('click',  function(event) {
 		$(".dropdown-toggle").removeClass('active');
 		$(this).addClass('active');
-	})
+	});
 })
